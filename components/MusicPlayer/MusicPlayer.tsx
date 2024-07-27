0cb3137f33e9907c5feb49e610d937191b85281e
Cyprien De Roef <c.deroef@outlook.fr>
2024-07-28 00:24:36 +0200

    Init MusicPlayer component

---
+++ components/MusicPlayer/MusicPlayer.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+
+interface MusicPlayerProps {}
+
+const MusicPlayer: FC<MusicPlayerProps> = () => (
+  <div>
+    MusicPlayer Component
+  </div>
+);
+
+export default MusicPlayer;
