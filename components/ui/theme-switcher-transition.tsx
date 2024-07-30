73617c54da3d84ad7310daadd89b2a79e87f16c9
Cyprien De Roef <c.deroef@outlook.fr>
2024-07-30 12:05:01 +0200

    Init ThemeSwitcherTransition component

---
+++ components/ui/theme-switcher-transition.tsx
@@ -0,0 +1,74 @@
+"use client";
+import * as Switch from "@radix-ui/react-switch";
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { flushSync } from "react-dom";
+
+export function ThemeSwitcherTransition() {
+    const [isDarkMode, setIsDarkMode] = useState(true);
+    const ref = useRef(null);
+
+    const toggleDarkMode = async (isDarkMode: boolean) => {
+        /**
+         * Return early if View Transition API is not supported
+         * or user prefers reduced motion
+         */
+        if (
+            !ref ||
+            // @ts-ignore
+            !document.startViewTransition
+        ) {
+            setIsDarkMode(isDarkMode);
+            return;
+        }
+
+        // @ts-ignore
+        await document.startViewTransition(() => {
+            flushSync(() => {
+                setIsDarkMode(isDarkMode);
+            });
+        }).ready;
+
+        // @ts-ignore
+        const { top, left, width, height } = ref.current.getBoundingClientRect();
+        const x = left + width / 2;
+        const y = top + height / 2;
+        const right = window.innerWidth - left;
+        const bottom = window.innerHeight - top;
+        const maxRadius = Math.hypot(Math.max(left, right), Math.max(top, bottom));
+
+        document.documentElement.animate(
+            {
+                clipPath: [
+                    `circle(0px at ${x}px ${y}px)`,
+                    `circle(${maxRadius}px at ${x}px ${y}px)`,
+                ],
+            },
+            {
+                duration: 500,
+                easing: "ease-in-out",
+                pseudoElement: "::view-transition-new(root)",
+            },
+        );
+    };
+
+    useEffect(() => {
+        if (isDarkMode) {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    }, [isDarkMode]);
+
+    return (
+        <Switch.Root checked={ isDarkMode } onCheckedChange={ toggleDarkMode }>
+            <Switch.Thumb ref={ ref }>
+                { isDarkMode ? (
+                    <Moon className="text-primary" />
+                ) : (
+                    <Sun className="text-primary" />
+                ) }
+            </Switch.Thumb>
+        </Switch.Root>
+    );
+}
