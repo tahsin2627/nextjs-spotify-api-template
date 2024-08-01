f5f8ab479bfcf8599179645b93bd79808431d6ba
Cyprien De Roef <c.deroef@outlook.fr>
2024-08-01 09:35:33 +0200

    Styles like ghost button

--- components/ui/theme-switcher-transition.tsx
+++ components/ui/theme-switcher-transition.tsx
@@ -61,12 +61,12 @@ export function ThemeSwitcherTransition() {
     }, [isDarkMode]);
 
     return (
-        <Switch.Root checked={ isDarkMode } onCheckedChange={ toggleDarkMode }>
+        <Switch.Root checked={ isDarkMode } onCheckedChange={ toggleDarkMode } className="h-10 px-4 py-2 transition-color rounded-md transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground">
             <Switch.Thumb ref={ ref }>
                 { isDarkMode ? (
-                    <Moon className="text-primary" />
+                    <Moon />
                 ) : (
-                    <Sun className="text-primary" />
+                    <Sun />
                 ) }
             </Switch.Thumb>
         </Switch.Root>
