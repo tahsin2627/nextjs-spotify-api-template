c33925e86ca7305f132a97dbb1a110887f67d94c
Cyprien De Roef <c.deroef@outlook.fr>
2024-08-01 12:22:37 +0200

    Added new .ico domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['i.scdn.co', 'thisis-images.spotifycdn.com', 'wrapped-images.spotifycdn.com', 'mosaic.scdn.co', 'blend-playlist-covers.spotifycdn.com', 'newjams-images.scdn.co', 'pl.scdn.co', 'charts-images.scdn.co', 'daily-mix.scdn.co', 'seeded-session-images.scdn.co', 'image-cdn-fa.spotifycdn.com', 'image-cdn-ak.spotifycdn.com', 'lineup-images.scdn.co'],
+        domains: ['i.scdn.co', 'thisis-images.spotifycdn.com', 'wrapped-images.spotifycdn.com', 'mosaic.scdn.co', 'blend-playlist-covers.spotifycdn.com', 'newjams-images.scdn.co', 'pl.scdn.co', 'charts-images.scdn.co', 'daily-mix.scdn.co', 'seeded-session-images.scdn.co', 'image-cdn-fa.spotifycdn.com', 'image-cdn-ak.spotifycdn.com', 'lineup-images.scdn.co', 'i2o.scdn.co'],
     },
 };
 
